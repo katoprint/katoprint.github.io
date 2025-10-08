33946def18a73e0d31e5f4a0d77414d063deedcb
katoprint <cskato2@gmail.com>
2025-10-07 17:24:44 -1000

    Add files via upload

--- navBar.js
+++ navBar.js
@@ -4,6 +4,6 @@ function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("navBar").style.padding = "2px 2px";
   } else {
-    document.getElementById("navBar").style.padding = "10px 10px";
+    document.getElementById("navBar").style.padding = "8px 8px";
   }
 }
